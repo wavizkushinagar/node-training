bfc3060c1da1b8578aec91bfb0e6c764f6da8480
Maneesh Kumar <maneesh-k@hcl.com>
2022-04-10 10:22:17 +0530

    Added review comment

--- beginner/login/ravi/index.js
+++ beginner/login/ravi/index.js
@@ -19,7 +19,7 @@ app.get('/login', function (req, res) {
   if (error) throw error;
   return res.send({ error: false, data: results, message: 'users list.' });
     
-    });
+  });
     });
 
     // Add a new user  
@@ -30,7 +30,7 @@ app.post('/user', function (req, res) {
   * @Ravi Do not use id
   */
    let data = {
-     id:req.body.id,  // @Ravi Comment this line and run
+   //  id:req.body.id,  // @Ravi Comment this line and run
      name:req.body.name,
      email:req.body.email,
      password :req.body.password
@@ -40,16 +40,31 @@ app.post('/user', function (req, res) {
   if (!data) {    
   return res.status(400).send({ error:true, message: 'Please provide user details' });
   }
+
+  const {name, email, password} = req.body
+  if (name && password &&  email) { 
+    
+    // isValidEmail is some custom email function to validate email which you might need write on your own or use npm module
+   
+    dbConn.query("INSERT INTO login SET ? ", data, function (error, results, fields) {
+      if (error) {
+        return res.status(400).send({ error:true, message: 'Something went wrong' });
+      }
+      return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
+      });
+      
+  }
+  else{
+    return res.status(204).send({ error:true, message: 'Validation error: Required field is missing' });
+  }
+
+});
   
   /**
    * @Ravi Validate Name, email and password it should not be undefined or empty
    */
 
-  dbConn.query("INSERT INTO login SET ? ", data, function (error, results, fields) {
-  if (error) throw error;
-    return res.send({ error: false, data: results, message: 'New user has been created successfully.' });
-  });
-  });
+
   
   
 app.listen(port, () => {
